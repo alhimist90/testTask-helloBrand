7bc8416f7569283a6863f6ddda975eafd0acec50
Daniil <alhimist90@yandex.ru>
2023-02-01 01:05:47 +0800

    Updates

--- mail.php
+++ mail.php
@@ -1,53 +1,55 @@
 <?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
 
-  // Файлы phpmailer
-  require 'phpmailer/PHPMailer.php';
-  require 'phpmailer/Exception.php';
+$title = "Тема письма";
 
-  $mail = new PHPMailer(true);
-  $mail->CharSet = 'UTF-8';
-  $mail->setLanguage('ru', 'phpmailer/language/');
-  $mail->IsHTMLl(true);
-
-  $mail->setFrom('info@yandex.ru', 'Неизвестный юзер');
-  $mail->addAddress('alhimist90@yandex.ru');
-  $mail->Subject = 'Тестовое письмо';
-
-  $hand = "Правая";
-  if($_POST['hand'] == "left") {
-    $hand = "Левая"
+$c = true;
+// Формирование самого письма
+$title = "Заголовок письма";
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+    $body .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
   }
+}
 
-  $body = '<h1>Письмо</h1>';
+$body = "<table style='width: 100%;'>$body</table>";
 
-  if(trim(!empty($_POST['name']))) {
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-  }
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
 
-  if(trim(!empty($_POST['email']))) {
-    $body.='<p><strong>E-Mail:</strong> '.$_POST['email'].'</p>';
-  }
+try {
+  $mail->isSMTP();
+  $mail->CharSet = "UTF-8";
+  $mail->SMTPAuth   = true;
 
-  if(trim(!empty($_POST['hand']))) {
-    $body.='<p><strong>E-Mail:</strong> '.$hand.'</p>';
-  }
+  // Настройки вашей почты
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = ''; // Логин на почте
+  $mail->Password   = ''; // Пароль на почте
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
 
-  if(trim(!empty($_POST['message']))) {
-    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
-  }
+  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
+  // Получатель письма
+  $mail->addAddress('');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
   $mail->Body = $body;
 
-  if (!$mail->send()) {
-    $message = 'Ошибка';
-  } else {
-    $message = 'Данные отправлены';
-  }
+  $mail->send();
 
-  $response = ['message' => $message];
+} catch (Exception $e) {
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
 
-  header('Content-type: application/json');
-  echo json_encode($response);
-?>
